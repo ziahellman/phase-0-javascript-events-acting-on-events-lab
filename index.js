bd8481d71c4f90b2e721a15fd2b7c175b87b892c
Zia Hellman <ziahellman@gmail.com>
2022-05-06 10:24:06 +0300

    commit tree aafa511115a07e68acdfbfab272b285f0f293f8f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
